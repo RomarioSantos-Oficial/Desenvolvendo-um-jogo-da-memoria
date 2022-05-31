c37e29eeab1da32ecb86ef51d5ff4d8e0605fc94
Romario Santos <limaromario40@gmail.com>
2022-05-31 12:39:42 -0300

    jogo da memoria pronto
    Jogo da memoria esta pronto

--- scripts.js
+++ scripts.js
@@ -1,8 +1,60 @@
 const cards = document.querySelectorAll('.card');
+let hasFlipperCard = false;
+let firstCard, secondCard;
+let locBoard = false
 
 function flipCard () {
-    this.classList.add('flip')
+    if (locBoard) return;
+    if(this== firstCard) return;
+    this.classList.add('flip');
+    if(!hasFlipperCard) {
+        hasFlipperCard =true;
+        firstCard =this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlipperCard = false;
+    checkForMath();
 }
+
+function checkForMath() {
+    if(firstCard.dataset.card === secondCard.dataset.card) {
+        disabledCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard ();
+}
+
+function unflipCards() {
+    locBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard ();
+    }, 1500)
+}
+
+function resetBoard(){
+    [hasFlipperCard, locBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+
+(function shuffle() {
+    cards.forEach((card) => {
+        let ramdomPosition = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPosition;
+    })
+}) ();
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
